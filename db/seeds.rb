9004530d49f20b379d5e91d8f5246ded0e85cddb
Sherwood <shcallaway@gmail.com>
2016-11-17 10:20:12 -0500

    Removed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Track.create({ name: 'Hello World', artist: 'Hot Artist', release: 'Self-Titled' })
