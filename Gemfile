3492d9c88b5cf896340691759d60d21802dc4268
Sherwood <shcallaway@gmail.com>
2016-11-17 10:14:13 -0500

    Improved names of environment variables

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 gem 'carrierwave', '0.10.0'
 gem "fog"
 gem 'dotenv-rails'
-# gem 'jbuilder', '~> 2.0' 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -4,9 +4,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # replace with ENV[''] before committing
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # replace with ENV[''] before committing
-    region: ENV['REGION'] 
+    region: ENV['AWS_REGION'] 
   }
 
-  config.fog_directory = ENV['S3_BUCKET'] # bucket name
+  config.fog_directory = ENV['AWS_S3_BUCKET'] # bucket name
 
 end
