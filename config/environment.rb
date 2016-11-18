90e52be523d2258b178f3abdf00450af11ec93d6
Sherwood <shcallaway@gmail.com>
2016-11-18 13:43:34 -0500

    Added root to JSON for best practice

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Include the resource name in JSON.
+ActiveRecord::Base.include_root_in_json = true
