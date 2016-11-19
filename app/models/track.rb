76ba11b97408ead69d5d5a986c249d87200fde68
Sherwood <shcallaway@gmail.com>
2016-11-19 18:03:03 -0500

    Fixed JSON formatting by overriding serializer file method

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,13 +3,13 @@ class Track < ApplicationRecord
   mount_uploader :file, TrackUploader
 
   # Validate fields.
-  validate :file_size
+  validate :max_file_size
   validates :name, :file, :artist, :release, presence: true
 
   private
 
   # Add an error if file is larger than limit.
-  def file_size
+  def max_file_size
     errors.add :file, "exceeds maximum size" if file.size > 10.megabytes
   end
 end
--- app/serializers/track_serializer.rb
+++ app/serializers/track_serializer.rb
@@ -1,3 +1,9 @@
 class TrackSerializer < ActiveModel::Serializer
   attributes :id, :name, :artist, :release, :file
+
+  # Override the file attribute serializer
+  # to produce better formatted JSON.
+  def file
+    object.file.url
+  end
 end
